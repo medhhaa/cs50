ed5b168750919be4306ed560a9b5b456a298b832
medhhaa <khatrik001@gmail.com>
2023-05-22 20:46:35 +0530

    Update credit.c
    with style50 guidelines

--- Week 1 - C/Problem Set 1/credit.c
+++ Week 1 - C/Problem Set 1/credit.c
@@ -15,7 +15,8 @@ int main(void)
     int valid = checksum(c);
     // printf("%i\n",valid);
 
-    if(valid == 0){
+    if (valid == 0)
+    {
         printf("INVALID\n");
     }
     else
@@ -28,7 +29,7 @@ int main(void)
 int digit_count(long c)
 {
     int count = 0;
-    while(c>0)
+    while (c > 0)
     {
         count++;
         c /= 10;
@@ -42,40 +43,53 @@ int digit_count(long c)
 void print_card(long c)
 {
     int count = digit_count(c);
-    if(count == 15){
+    if (count == 15)
+    {
         //American
         c /= 10000000000000;
-        if(c == 34 || c == 37)
+        if (c == 34 || c == 37)
         {
             printf("AMEX\n");
-        }else{printf("INVALID\n");}
+        }
+        else
+        {
+            printf("INVALID\n");
+        }
     }
-    else if(count == 16)
+    else if (count == 16)
     {
         //Master OR Visa
         c /= 100000000000000;
-        if(c == 51 || c == 52 || c == 53 || c == 54 || c == 55)
+        if (c == 51 || c == 52 || c == 53 || c == 54 || c == 55)
         {
             printf("MASTERCARD\n");
         }
         else
         {
-            c/=10;
-            if(c == 4)
+            c /= 10;
+            if (c == 4)
             {
                 printf("VISA\n");
-            }else{printf("INVALID\n");}
+            }
+            else
+            {
+                printf("INVALID\n");
+            }
         }
     }
-    else if(count == 13)
+    else if (count == 13)
     {
         //Visa
         c /= 1000000000000;
         // printf("%li \n", c);
-        if(c == 4)
+        if (c == 4)
         {
             printf("VISA\n");
-        }else{printf("INVALID\n");}
+        }
+        else
+        {
+            printf("INVALID\n");
+        }
     }
     else
     {
@@ -86,9 +100,9 @@ void print_card(long c)
 int digit_sum(int p) //calculate sum of number given as argument
 {
     int sum = 0;
-    while(p>0)
+    while (p > 0)
     {
-        sum += (p%10);
+        sum += (p % 10);
         p /= 10;
     }
     return sum;
@@ -101,32 +115,34 @@ int checksum(long c)
     //3. add both of these and check modulo for 0
 
     int sum_last = 0, sum_alt = 0, digit, flag = 0, sum;
-    while(c>0)
+    while (c > 0)
     {
-       digit = c % 10;
-       c /= 10;
-       if(flag == 0)  //checking flag for last or second last
-       {
+        digit = c % 10;
+        c /= 10;
+        if (flag == 0)  //checking flag for last or second last
+        {
             sum_last += digit;
             flag = 1;
             //printf("LAST : digit: %i, sum: %i\n", digit, sum_last );
-       }
-       else
-       {
-            sum = digit_sum(digit*2); //take sum of digits and not digit itself!
+        }
+        else
+        {
+            sum = digit_sum(digit * 2); //take sum of digits and not digit itself!
             sum_alt += (sum);
             flag = 0;
             //printf("ALT : digit: %i, sum: %i\n", digit, sum_alt );
-       }
+        }
     }
     int add = sum_last + sum_alt;
     //printf("last %i alt  %i  sum %i\n",sum_last, sum_alt, add);
 
-    if(add%10 == 0)
+    if (add % 10 == 0)
     {
-            return 1;
-    }else{
-            return 0;
+        return 1;
+    }
+    else
+    {
+        return 0;
     }
     return 0;
 }
