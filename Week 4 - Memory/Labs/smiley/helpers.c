d93252febb89d17b45fe5538331bc41c767a3e76
medhhaa <khatrik001@gmail.com>
2023-06-03 17:25:20 +0530

    smiley :)

--- Week 4 - Memory/Labs/smiley/helpers.c
+++ Week 4 - Memory/Labs/smiley/helpers.c
@@ -3,5 +3,17 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
-    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
+            {
+                image[i][j].rgbtRed = 0x0A;
+                image[i][j].rgbtBlue = 0xFB;
+                image[i][j].rgbtGreen = 0x9E;
+            }
+        }
+    }
+
 }
