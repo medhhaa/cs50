02d9e2d4a79c0f489e7c9f1abca8c9172cfa55a2
medhhaa <khatrik001@gmail.com>
2023-06-22 18:44:20 +0530

    filter-more

---
+++ Week 4 - Memory/Problem Set/filter-more/helpers.c
@@ -0,0 +1,33 @@
+#include "helpers.h"
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    //take average of rgb values and turn the pixel to avg value
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
