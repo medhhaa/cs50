5f236d3494efe97dbf9876d1cf991aa097e8b76b
medhhaa <khatrik001@gmail.com>
2023-07-19 21:35:50 +0530

    Update readability.py

--- Week 6 - Python/Problem Sets/readability.py
+++ Week 6 - Python/Problem Sets/readability.py
@@ -4,66 +4,51 @@ from cs50 import get_string
 def main():
     text = get_string("Text: ")
     #count letters
-    letters = count_letters(text) 
+    letters = count_letters(text)
 
     #count words
-    words = count_words(text) 
+    words = count_words(text)
 
     #count sentences
-    sentences = count_sentences(text) 
+    sentences = count_sentences(text)
+
+    # print(letters, words, sentences)
 
     #index = 0.0588 * L - 0.296 * S - 15.8
-    L = (letters // () words) * 100 
-    S = (sentences // () words) * 100 
+    L = (letters //  words) * 100
+    S = (sentences //  words) * 100
 
-    g = 0.0588 * L - 0.296 * S - 15.8 
-    grade = round(g) 
+    g = 0.0588 * L - 0.296 * S - 15.8
+    grade = round(g)
 
-    # print("%f %f %f %i", L, S, g, grade) 
+    # print(L, S, g, grade)
 
     if (grade < 2):
-        print("Before Grade 1\n") 
+        print("Before Grade 1\n")
     elif (grade > 16):
-        print("Grade 16+\n") 
+        print("Grade 16+\n")
     else:
-        print("Grade ", grade) 
+        print("Grade ", grade)
 
 def count_letters(text):
     n = 0
-    letter = 0 
-    for let in text.upper(): #Because OOP, you can directly call fucntions 
+    letter = 0
+    for let in text.upper(): #Because OOP, you can directly call fucntions
         if (let >= 'A' and let <= 'Z'):
-            letter += 1 
+            letter += 1
         n += 1
-    return letter 
+    return letter
 
-   count_words(string text)
-{
-       n = 0, words = 1 
-    while (text[n] != '\0')
-    {
-        text[n] = toupper(text[n]) 
-        if (text[n] == ' ')
-        {
-            words += 1 
-        }
-        n += 1 
-    }
-     # print("%i words\n", words) 
-    return words 
-}
-   count_sentences(string text)
-{
-       n = 0, sentences = 0 
-    while (text[n] != '\0')
-    {
-        text[n] = toupper(text[n]) 
-        if (text[n] == '.' || text[n] == '?' || text[n] == '!')
-        {
-            sentences += 1 
-        }
-        n += 1 
-    }
-     # print("%i sentences\n", sentences) 
-    return sentences 
-}
+def count_words(text):
+    # words = 1
+    words = text.split()
+    return len(words)
+
+def count_sentences(text):
+    sentences = 0
+    for let in text.upper():
+        if (let == '.' or let == '?' or let == '!'):
+            sentences += 1
+    return sentences
+
+main()
